ca0c6cfc185d5c7e9cdb90462b4e0b010b0d0e46
haidarsdq <68592280+haidar-sdq@users.noreply.github.com>
2022-03-15 13:31:36 +0530

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Java Container Running in Github Actions!";
+        return "Hello from Java Container Running in Github Actions!!";
     }
 
 }
